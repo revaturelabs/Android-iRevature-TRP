43abd887a5a19d879848059cde175edbd520b101
Thiagoob <tbarb016@fiu.edu>
2020-02-13 14:19:32 -0500

    Modified trainers and batches xml

--- RevatureTrainingRoomPlanner/app/src/main/java/com/revature/revaturetrainingroomplanner/ui/rooms/RoomsFragment.java
+++ RevatureTrainingRoomPlanner/app/src/main/java/com/revature/revaturetrainingroomplanner/ui/rooms/RoomsFragment.java
@@ -4,13 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
 
 import com.revature.revaturetrainingroomplanner.R;
 
@@ -20,11 +16,7 @@ public class RoomsFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        roomsViewModel =
-                ViewModelProviders.of(this).get(RoomsViewModel.class);
         View root = inflater.inflate(R.layout.fragment_rooms, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        roomsViewModel.getText().observe(getViewLifecycleOwner(), s -> textView.setText(s));
         return root;
     }
 }
