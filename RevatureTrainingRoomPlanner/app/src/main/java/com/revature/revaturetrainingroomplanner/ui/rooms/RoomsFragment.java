ea362957e481db5a6a7ed19be547866e90a626e4
Phirom Peterschmidt <phiromp@gmail.com>
2020-02-13 21:41:53 -0500

    full assignment path

--- RevatureTrainingRoomPlanner/app/src/main/java/com/revature/revaturetrainingroomplanner/ui/rooms/RoomsFragment.java
+++ RevatureTrainingRoomPlanner/app/src/main/java/com/revature/revaturetrainingroomplanner/ui/rooms/RoomsFragment.java
@@ -7,22 +7,29 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import com.revature.revaturetrainingroomplanner.R;
 import com.revature.revaturetrainingroomplanner.ui.adapter.RoomsAdapter;
 
+import java.util.Objects;
+
 public class RoomsFragment extends Fragment implements RoomsAdapter.OnItemListener {
 
     private RoomsViewModel roomsViewModel;
+    private NavController mNavController;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_rooms, container, false);
+        mNavController = Navigation.findNavController(Objects.requireNonNull(getActivity()), R.id.nav_host_fragment);
         return root;
     }
 
     @Override
     public void onItemClick(int position) {
-
+        Bundle args = new Bundle();
+        mNavController.navigate(R.id.nav_rooms_info, args);
     }
 }
--- RevatureTrainingRoomPlanner/app/src/main/java/com/revature/revaturetrainingroomplanner/ui/trainers/TrainersInfoFragment.java
+++ RevatureTrainingRoomPlanner/app/src/main/java/com/revature/revaturetrainingroomplanner/ui/trainers/TrainersInfoFragment.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.view.LayoutInflater;
@@ -21,12 +23,14 @@ import com.revature.revaturetrainingroomplanner.R;
 import com.revature.revaturetrainingroomplanner.ui.batches.BatchesFragment;
 import com.revature.revaturetrainingroomplanner.ui.trainers.TrainersFragment;
 
+import java.util.Objects;
+
 /**
  * A simple {@link Fragment} subclass.
  */
 public class TrainersInfoFragment extends Fragment implements View.OnClickListener{
 
-
+    NavController mNavController;
 
     public TrainersInfoFragment() {
         // Required empty public constructor
@@ -37,6 +41,8 @@ public class TrainersInfoFragment extends Fragment implements View.OnClickListen
                              Bundle savedInstanceState) {
 
         View rootView = inflater.inflate(R.layout.fragment_trainer_info, container, false);
+        mNavController = Navigation.findNavController(Objects.requireNonNull(getActivity()), R.id.nav_host_fragment);
+
         Button assign = rootView.findViewById(R.id.btn_trainer_info_assign);
         assign.setOnClickListener(this);
 
@@ -47,6 +53,8 @@ public class TrainersInfoFragment extends Fragment implements View.OnClickListen
     public void onClick(View v) {
 
         Toast.makeText(getContext(), "Trainer assigned to batch", Toast.LENGTH_LONG).show();
+        Bundle args = new Bundle();
+        mNavController.navigate(R.id.nav_rooms_info, args);
 
     }
 }
